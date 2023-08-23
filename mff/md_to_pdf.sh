46f618657be7f0ad9f5fa7612c03a6063b599eac
Tomáš Turek <tom.turek@atlas.cz>
2023-08-23 16:10:06 +0200

    update

--- mff/md_to_pdf.sh
+++ mff/md_to_pdf.sh
@@ -15,13 +15,11 @@ Občas stihnu sepsat nějaké zápisky z přednášky a pokud stojí za to, tak 
 ## Zápisky
 " > $mff
 
-for file in *; do
-	if [[ $file == *.md ]]; then
-		#html=$( echo $file | sed 's/\.md/\.html/g' )
-		pdf=$( echo $file | sed 's/\.md/\.pdf/g')
-		#pandoc --katex $file -o $html
-		pandoc --katex $file -o $pdf
-		name=$(grep "title: " $file | sed 's/title: //g')
-		echo "- [$name]($pdf)" >> $mff
-	fi
+for file in $(ls *.md); do
+	#html=$( echo $file | sed 's/\.md/\.html/g' )
+	pdf=$( echo $file | sed 's/\.md/\.pdf/g')
+	#pandoc --katex $file -o $html
+	pandoc --katex $file -o $pdf
+	name=$(grep "title: " $file | sed 's/title: //g')
+	echo "- [$name]($pdf)" >> $mff
 done
